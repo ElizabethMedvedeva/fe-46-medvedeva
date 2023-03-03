33c7ee7c514b35d2ebfa33a86e6666c27b6b10a4
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2023-03-03 14:10:22 +0200

    make tab switchable

--- axit-project/tab.js
+++ axit-project/tab.js
@@ -1,12 +1,32 @@
 const links = document.querySelectorAll(".tabs__nav-link");
 
 const onWindowClick = (e) => {
-    if (e.target.tagName === 'A'){
-        e.target.parentNode.click()
-    }
+  if (e.target.tagName === "A") {
+    e.target.parentNode.click();
+  }
 };
 
-window.addEventListener('click', onWindowClick, false)
+window.addEventListener("click", onWindowClick, false);
 window.onload = function () {
-    links[0].click();
+  links[0].click();
 };
+
+const tabs = document.querySelectorAll(".tabs__nav-item");
+const contents = document.querySelectorAll(".tab");
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", (event) => {
+    let tabsChildren = event.target.parentElement.children;
+    for (let t = 0; t < tabsChildren.length; t++) {
+      tabsChildren[t].classList.remove("tabs__nav-item_active");
+    }
+    tabs[i].classList.add("tabs__nav-item_active");
+
+    let tabContentChildren =
+      event.target.parentElement.nextElementSibling.children;
+    for (let c = 0; c < tabContentChildren.length; c++) {
+      tabContentChildren[c].classList.remove("tab__content_active");
+    }
+    contents[i].classList.add("tab__content_active");
+  });
+}
